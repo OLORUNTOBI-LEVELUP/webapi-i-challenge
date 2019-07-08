698209db69850274625068b1aef7e8aac2c2ad6c
oloruntobi <awoderuoloruntobi@gmail.com>
2019-07-08 13:30:27 +0100

    DELETE user by ID

--- index.js
+++ index.js
@@ -37,7 +37,22 @@ app.get('/api/users/:id' ,(req, res) => {
          .json({ error: "The user information could not be retrieved." })
      })
      
-        
+    app.delete('/api/users/:id', (req,res) => {
+        const { id } = req.params;
+        users.remove(id)
+        .then(user => {
+            if (user) {
+              res.status(200).json({ message: 'The user was sucessfully deleted' })
+            } else {
+              res
+                .status(404)
+                .json({ message: 'The user with the specified ID does not exist.' })
+            }
+          })
+          .catch(() =>
+            res.status(500).json({ error: 'The user could not be removed' })
+          )
+      })
         
         
             
